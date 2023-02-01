2d7f4a5e6254caeac72f405e18d3f86d747b16a6
Mathew Browne <mathewbrowne@gmail.com>
2023-02-01 12:36:46 +0000

    Create a function to check a timestamp input is valid

---
+++ inc/functions.php
@@ -0,0 +1,10 @@
+<?php
+
+function isValidTimeStamp($timestamp)
+{
+    return ((string) (int) $timestamp === $timestamp)
+        && ($timestamp <= PHP_INT_MAX)
+        && ($timestamp >= ~PHP_INT_MAX);
+}
+
+?>
